eb4663fc2ad23451da5cadb88b665d15669138c5
Makoto Tanabe <xekid78@outlook.com>
2018-03-12 20:40:58 +0900

    Add files via upload

--- 2darray.rb
+++ 2darray.rb
@@ -1,23 +1,23 @@
 team = [["佐藤","鈴木","田中"],["岸田","有森","峯川"],["武田","武藤","神田"]]
 p team
-team.length
+puts "チーム数は#{team.length}チームです。"
 puts ""
 
 puts "*** 追加 ***"
 team.push(["芥川","太宰","直木"])
 team[0].push("田沢")
 p team
-team.length
+puts "チーム数は#{team.length}チームです。"
 puts ""
 
 puts "*** 変更 ***"
 team[0][2] = "佐川"
 p team
-team.length
+puts "チーム数は#{team.length}チームです。"
 puts ""
 
 puts "*** 削除 ***"
 team.delete_at(3)
 team[2].delete_at(1)
 p team
-team.length
+puts "チーム数は#{team.length}チームです。"
